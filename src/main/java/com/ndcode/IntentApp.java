a683db704551c2d9328196c4879463fc63d12e85
Noman Ud Din <din@beontra.com>
2020-10-09 23:12:04 +0200

    Intent: Change state of running program

--- src/main/java/com/ndcode/IntentApp.java
+++ src/main/java/com/ndcode/IntentApp.java
@@ -13,33 +13,35 @@ public class IntentApp {
      * Application entry point.
      */
     public static void main(String[] args) {
-        BufferedReader questionReader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter your desired question: ");
-        try {
-            var question = questionReader.readLine();
+        while (true) {
+            BufferedReader questionReader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter your desired question: ");
+            try {
+                var question = questionReader.readLine();
 
-            if (question.isEmpty()) {
-                System.out.println("You did not enter a question!");
-            } else if (question.contains("weather")
-            ) {
-                WeatherIntent weatherIntent = new WeatherIntent();
-                weatherIntent.getWeather(question);
-            } else if (question.contains("free")
-                    || question.contains(":")
-                    || question.contains("appointment")
-            ) {
-                AppointmentIntent appointmentIntent = new AppointmentIntent();
-                appointmentIntent.checkCalendarForFreeSlot(question);
-            } else if (question.contains("fact")) {
-                FactIntent factIntent = new FactIntent();
-                factIntent.getInterestingFact();
-            } else System.out.println("Intent is not recognized please try again!");
+                if (question.isEmpty()) {
+                    System.out.println("You did not enter a question!");
+                } else if (question.contains("weather")
+                ) {
+                    WeatherIntent weatherIntent = new WeatherIntent();
+                    weatherIntent.getWeather(question);
+                } else if (question.contains("free")
+                        || question.contains(":")
+                        || question.contains("appointment")
+                ) {
+                    AppointmentIntent appointmentIntent = new AppointmentIntent();
+                    appointmentIntent.checkCalendarForFreeSlot(question);
+                } else if (question.contains("fact")) {
+                    FactIntent factIntent = new FactIntent();
+                    factIntent.getInterestingFact();
+                } else System.out.println("Intent is not recognized please try again!");
 
-        } catch (IOException e) {
-            e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("");
+            System.out.println("");
         }
-
     }
-
 }
 
