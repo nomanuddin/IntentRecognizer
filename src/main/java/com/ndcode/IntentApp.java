99886c1c4f99312eba0ccef4d0d16c252b811574
Noman Ud Din <din@beontra.com>
2020-10-10 21:22:01 +0200

    Intent: Added calendar check logic
    -- api logic is not implemented.
    current class can be extended further

--- src/main/java/com/ndcode/IntentApp.java
+++ src/main/java/com/ndcode/IntentApp.java
@@ -14,27 +14,42 @@ public class IntentApp {
      * Application entry point.
      */
     static Logger logger = Logger.getLogger(IntentApp.class.getName());
+
     public static void main(String[] args) {
 
         while (true) {
+            // defining reader for command line
             BufferedReader questionReader = new BufferedReader(new InputStreamReader(System.in));
             System.out.print("Enter your desired question: ");
+
+            // getting user question and parsing it
             try {
+                //reading question from terminal
                 var question = questionReader.readLine();
 
+                //TODO: Following if conditions can be extended switch statements
+                //TODO: For using switch statements better option would be to
+                //TODO: develop a standard parsing/regex algo which can optimize the logic further
                 if (question.isEmpty()) {
                     System.out.println("You did not enter a question!");
-                } else if (question.contains("weather")
+                }
+                //if question contain weather keyword then respective class is called
+                //for further processing
+                else if (question.contains("weather")
                 ) {
                     WeatherIntent weatherIntent = new WeatherIntent();
                     weatherIntent.getWeather(question);
-                } else if (question.contains("free")
-                        || question.contains(":")
+                }
+                //if question contain free or appointment keywords then respective class is called
+                //for further processing
+                else if (question.contains("free")
                         || question.contains("appointment")
                 ) {
                     AppointmentIntent appointmentIntent = new AppointmentIntent();
-                    appointmentIntent.checkCalendarForFreeSlot(question);
-                } else if (question.contains("fact")) {
+                    appointmentIntent.checkAppointment(question);
+                }//if question contain fact keyword then respective class is called
+                //for further processing
+                else if (question.contains("fact")) {
                     FactIntent factIntent = new FactIntent();
                     factIntent.getInterestingFact();
                 } else System.out.println("Intent is not recognized please try again!");
--- src/main/java/com/ndcode/intentrecognizer/AppointmentIntent.java
+++ src/main/java/com/ndcode/intentrecognizer/AppointmentIntent.java
@@ -1,9 +1,42 @@
 package com.ndcode.intentrecognizer;
 
+import java.util.Random;
+
 public class AppointmentIntent {
 
-    public void checkCalendarForFreeSlot(String question) {
+    //checkAppointments using sub-methods of claas find the time
+    //and day from given query and check display random output
+    public void checkAppointment(String question) {
         System.out.println("Intent: Check calendar");
+
+        // parsing question for time slot and day
+        var timeSlotRequested = getSlotFromQuestion(question);
+        var dayRequested = getDayFromQuestion(question);
+
+        // checking calendar
+        var checkIfFree = checkCalendarForTimeAndDay(timeSlotRequested, dayRequested);
+
+        System.out.println("Free: " + (checkIfFree ? "Yes" : "No"));
     }
 
+    // getting time from question
+    private String getSlotFromQuestion(String question) {
+        //TODO: Logic for getting time from question
+        return "timeSlot";
+    }
+
+    // getting day from question
+    private String getDayFromQuestion(String question) {
+        //TODO: Logic for getting day from question
+        return "day";
+    }
+
+    // checking calendar for time and day provided
+    private Boolean checkCalendarForTimeAndDay(String time, String day) {
+        //TODO: Check using calendar api the requested time and day
+
+        //Return random true/false value for a prototype implementation
+        Random random = new Random();
+        return random.nextBoolean();
+    }
 }
